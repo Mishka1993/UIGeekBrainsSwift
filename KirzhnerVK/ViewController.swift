a87909161a6cbc155f9b27bb81cab889b95e5e92
Michail <misha220993@gmail.com>
2021-10-28 23:33:43 +0400

    Lesson_1

---
+++ KirzhnerVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KirzhnerVK
+//
+//  Created by Екатерина on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Жест нажатия
+             
+    
+    }
+
+}
